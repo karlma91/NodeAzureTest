9f7387b6a5b0fefb483e14ba47adbe030f42bdeb
karlma91 <km.kalvik@gmail.com>
2016-07-12 14:51:21 +0200

    added webhook?

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ var app = express();
 
 require('./config/express')(app, config);
 
+// for Facebook verification
+app.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+        res.send(req.query['hub.challenge'])
+    }
+    res.send('Error, wrong token')
+})
+
 app.listen(config.port, function () {
   console.log('Express server listening on port ' + config.port);
 });
