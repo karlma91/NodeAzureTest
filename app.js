053478cca9437472eaf831d7c92476d550aa70b5
karlma91 <km.kalvik@gmail.com>
2016-07-13 14:28:41 +0200

    removed azure

--- app.js
+++ app.js
@@ -16,8 +16,7 @@ const
   crypto = require('crypto'),
   express = require('express'),
   https = require('https'),  
-  request = require('request'),
-  azure = require('azure-storage');
+  request = require('request');
 
 var app = express();
 app.set('port', process.env.PORT || 5000);
