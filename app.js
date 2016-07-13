d4b3fca7d65f31d71858345cd0921c4dbeda1288
karlma91 <km.kalvik@gmail.com>
2016-07-13 16:16:42 +0200

    asdsadddsad

--- app.js
+++ app.js
@@ -313,7 +313,7 @@ function receivedMessage(event) {
         break;
         
       default:
-        //sendTextMessage(senderID, messageText);
+        sendTextMessage(senderID, messageText);
     }
   } else if (messageAttachments) {
     sendTextMessage(senderID, "Message with attachment received");
